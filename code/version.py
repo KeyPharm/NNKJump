854a4a8dbb0fd27377d6e0f57afb68df8ab0ce0d
KeyPharm <102919247+KeyPharm@users.noreply.github.com>
2022-07-03 04:34:04 +0200

    Add files via upload

--- code/version.py
+++ code/version.py
@@ -18,7 +18,7 @@ import requests
 # save some space
 # save some space
 # respeta los espacios antes y después  del =. y siempre línea 20
-versioncontrol = "0.01"
+versioncontrol = "0.02"
 
 def update():
     raw = requests.get("https://raw.githubusercontent.com/KeyPharm/NNKJump/main/code/version.py").text
